83192b684cc12b6e2d02cc3888090895239ec2ef
Kayvin <kayvin4@hotmail.com>
2022-06-29 13:19:30 +0200

    feat(layout): add title and description meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import React from 'react'
 
 import '../styles/index.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Kayvin Ngueeping - Développeur Front-End React</title>
+        <meta
+          name="description"
+          content="Je suis développeur front-end depuis plus de 6 ans. J'habite à Lille. Bienvenue sur mon site"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} className="root" />{' '}
+    </>
+  )
 }
 
 export default MyApp
