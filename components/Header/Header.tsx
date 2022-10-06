61448e561912174bb830b4e20f2803a31cde73e0
Kayvin <kayvin4@hotmail.com>
2022-10-06 20:56:41 +0200

    feat(logo): create logo component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Logo from '../Logo/Logo'
+/* import Navbar from '../Navbar/Navbar' */
 import styles from './Header.module.scss'
 
 const Header = () => {
-  return <header className={styles.header} />
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <div>nav</div>
+      <div>burger</div>
+    </header>
+  )
 }
 
 export default Header
