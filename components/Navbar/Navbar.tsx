f7ac276541dba185d39262cfc9259c81f7484300
Kayvin <kayvin4@hotmail.com>
2022-11-15 01:18:29 +0100

    feat(navbar): create navbar component + style

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import styles from './Navbar.module.scss'
+
+type Props = {
+  isActive: boolean
+  onClick: () => void
+}
+
+const Navbar = ({ isActive, onClick }: Props) => {
+  const router = useRouter()
+  const currentRoute = router.pathname
+
+  return (
+    <nav className={`${styles.navbar} ${isActive ? styles.active : ''}`}>
+      <Link
+        onClick={onClick}
+        href="/#About"
+        className={currentRoute === '/about' ? styles.active : styles.nonActive}
+      >
+        <button type="button" onClick={onClick}>
+          About
+        </button>
+      </Link>
+      <Link
+        onClick={onClick}
+        href="/#Work"
+        className={currentRoute === '/Work' ? styles.active : styles.nonActive}
+      >
+        <button type="button" onClick={onClick}>
+          Work
+        </button>
+      </Link>
+      <Link
+        onClick={onClick}
+        href="/#Contact"
+        className={
+          currentRoute === '/Contact' ? styles.active : styles.nonActive
+        }
+      >
+        <button type="button" onClick={onClick}>
+          Contact
+        </button>
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
+
+/* ;<li className={router.asPath === '/#About' ? 'active' : ''}>
+  <Link href="/#About" replace>
+    About
+  </Link>
+</li>
+ */
