bcf153c3c5d189701525f78537623ad4682a66e0
Kayvin <kayvin4@hotmail.com>
2022-12-28 10:45:13 +0100

    feat(scrolldown): add react icon

--- components/ScrollDown/ScrollDown.tsx
+++ components/ScrollDown/ScrollDown.tsx
@@ -1,28 +1,18 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
-import styles from './ScrollDown.module.scss'
+import { AiOutlineArrowDown } from 'react-icons/ai'
 
-/* type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  id: string
-} */
+import styles from './ScrollDown.module.scss'
 
 const ScrollDown = () => {
   return (
     <Link href="/#About">
       <button className={styles.button} type="button">
-        <Image
-          className={styles.img}
-          src="/down-arrow.svg"
-          alt="An SVG of my logo"
-          width={60}
-          height={60}
-        />
-        <p>Scoll down</p>
+        <AiOutlineArrowDown />
+        <p>Scroll down</p>
       </button>
     </Link>
   )
-  /*   return <button type="button" id={id} /> */
 }
 
 export default ScrollDown
