d4fe91e2748271fd65bf6b88fd1ca7189127fe47
kayvin <kayvin4@hotmail.com>
2024-03-18 01:46:58 +0100

    fix: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,22 @@ import React from 'react'
 import './global.scss'
 
 export const metadata: Metadata = {
-  title: 'Home',
-  description: 'Welcome to Next.js'
+  title: 'Kayvin Ngueeping - Développeur Front-End React',
+  description:
+    "Je suis développeur front-end depuis 2016. J'habite à Lille. Bienvenue sur mon site",
+  openGraph: {
+    title: 'Kayvin Ngueeping - Développeur Front-End React',
+    description:
+      "Je suis développeur front-end depuis 2016. J'habite à Lille. Bienvenue sur mon site",
+    type: 'website',
+    siteName: 'Kayvin Ngueeping',
+    locale: 'fr_FR',
+    url: 'https://www.kayvin.fr/'
+  }
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
-  <html lang="en">
+  <html lang="fr">
     <body>{children}</body>
   </html>
 )
