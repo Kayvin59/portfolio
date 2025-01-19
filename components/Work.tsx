d7e82d296f051fd5395f4e58d135890f02273a25
Kayvin59 <kayvin4@hotmail.com>
2025-01-19 02:08:29 +0100

    fix: update img + style

--- components/Work.tsx
+++ components/Work.tsx
@@ -2,24 +2,26 @@ import { Card, CardContent } from "@/components/ui/card"
 import Image from 'next/image'
 
 const projects = [
-  { title: 'Project 1', subtitle: 'Web Application', image: '/kering.jpg' },
-  { title: 'Project 2', subtitle: 'Mobile App', image: '/kering.jpg' },
-  { title: 'Project 3', subtitle: 'E-commerce Platform', image: '/kering.jpg' },
-  { title: 'Project 4', subtitle: 'Dashboard Design', image: '/kering.jpg' },
+  { title: 'Happychic', subtitle: 'E-commerce Platform', image: '/happychic.jpg' },
+  { title: 'Loewe', subtitle: 'Blog', image: '/loewe.jpg' },
+  { title: 'LOréal', subtitle: 'E-commerce Platform', image: '/kering.jpg' },
+  { title: 'Kering', subtitle: 'E-commerce Platform', image: '/ysl.jpg' },
+  { title: 'GSMC', subtitle: 'SaaS', image: '/gsmc.jpg' },
+  { title: 'Whiteswap', subtitle: 'Exchange', image: '/exchange.jpg' },
 ]
 
 export default function Work() {
   return (
     <section id="work" className="py-12">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-8">Work</h2>
+        <h2 className="text-3xl font-abril text-center mb-8">Work</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8">
           {projects.map((project, index) => (
             <div 
               key={project.title} 
               className={`flex flex-col ${index % 2 !== 0 ? 'md:translate-y-16' : ''}`}
             >
-              <Card className="flex-grow overflow-hidden h-full">
+              <Card className="bg-background flex-grow overflow-hidden h-full border-0 rounded-none">
                 <CardContent className="p-0 flex flex-col h-full">
                   <div className="relative w-full pt-[75%]">
                     <Image
